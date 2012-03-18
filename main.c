1cd214e9002c2985837267f179d62b227e2653ef
Linden Ralph <lindenr@fastmail.fm>
2012-03-18 11:28:46 +0000

    more gravity

--- main.c
+++ main.c
@@ -7,18 +7,18 @@
 #define BIGGIFY(x,y) ((((Uint32)(x))<<16)|(y))
 #define NUM_FBs     100
 #define NUM_FLs     10
-#define SQUARE_DIST 625
+#define SQUARE_DIST 1000
+#define LEVEL_LENGTH 700
 //#define NO_FIREBALLS
 //#define PLAYER_EXPECTED
 //#define JUST_SHOW /* no generating of fireballs or anything*/
 
-int WIDTH=640, HEIGHT=480;
+int WIDTH=640, HEIGHT=480, EXIT_XLOC=LEVEL_LENGTH-30, EXIT_YLOC=200;
 int All_ticks = 0;
-int PLAYING = 1;
+int PLAYING = 0;
+int XDIST=20*20, YDIST=36*36;
 
-Uint8 floor_colours[3][3] = {{0, 255, 0}, {255, 0, 0}, {255, 0, 255}};
-
-SDL_Surface *fb_screen = NULL;
+SDL_Surface *fb_screen = NULL, *spiral_screen = NULL, *floor_screen = NULL, *door_screen = NULL, *player_screen;
 
 struct FireBall
 {
@@ -29,11 +29,18 @@ struct FireBall
 
 struct FireBall fireballs[NUM_FBs];
 
+struct Camera
+{
+    int xloc; /* The left-most corner of the screen */
+    int yloc; /* Ditto for vertical stuff */
+} camera = {100, 0};
+
 struct FloorSection
 {
     Sint16 xloc, yloc; /* where and how high, respectively */
     Sint16 length;
     int view;
+    SDL_Surface *surf;
 };
 
 struct FloorSection floors[NUM_FLs];
@@ -54,9 +61,7 @@ int is_too_close(Uint32 a, Uint32 b)
     Sint16 y1 = a&0xFFFF, x1 = a>>16;
     Sint16 y2 = b&0xFFFF, x2 = b>>16;
     Sint16 d1 = x1-x2, d2 = y1-y2;
-    Uint32 D = d1*d1 + d2*d2;
-//    fprintf(stderr, "D: %u  ", D);
-    return (D <= SQUARE_DIST);
+    return (d1*d1 < XDIST && d2*d2 < YDIST);
 }
 
 int get_jump_height(int start_speed, int time)
@@ -77,11 +82,11 @@ Uint32 proper_get_pos(struct Player *player, int ticks)
     if(ticks < player->rn3) return BIGGIFY(100+2*player->rn2, 100);
     ticks -= player->rn3;
     return BIGGIFY(100+2*player->rn2+2*ticks, 100);*/
-    if (ticks < 50) return BIGGIFY(100 + ticks*2, 200);
-    ticks -= 50;
-    if (ticks < 30) return BIGGIFY(200 + ticks*2, 200-get_jump_height(10, ticks));
-    ticks -= 30;
-    return BIGGIFY(260 + ticks*2, 200-get_jump_height(10,30));
+    if (ticks < player->rn1) return BIGGIFY(100 + ticks*2, 200);
+    ticks -= player->rn1;
+    if (ticks < player->rn2) return BIGGIFY(100 + player->rn1*2 + ticks*2, 200-get_jump_height(10, ticks));
+    ticks -= player->rn2;
+    return BIGGIFY(100 + (player->rn1+player->rn2)*2 + ticks*2, 200-get_jump_height(10,player->rn2));
 }
 
 Uint32 player_get_pos(struct Player *player)
@@ -154,6 +159,20 @@ void generate_floor(struct Player *player)
         if (fl.length < 7)
             continue;
         fl.yloc += (player->height >> 1)+1;
+        int x, y;
+        struct FloorSection *F;
+        F = &fl;
+        SDL_Surface *surf = SDL_CreateRGBSurface(SDL_SWSURFACE, F->length, HEIGHT - F->yloc, 32, 0, 0, 0, 0);
+        SDL_Rect rect = {0,0,0,0};
+        F->surf = surf;
+        for (x = 0; x < F->surf->w; x += floor_screen->w)
+        {
+            for (y = 0; y < F->surf->h; y += floor_screen->h)
+            {
+                rect.x = x; rect.y = y;
+                SDL_BlitSurface(floor_screen, NULL, F->surf, &rect);
+            }
+        }
         floors[f] = fl;
         ++ f;
     }
@@ -207,9 +226,11 @@ void player_draw(struct Player *player, SDL_Surface *screen)
     }
     else player_set_loc(player, proper_get_pos(player, All_ticks));
     if (there_is_wall(player)) player->xloc -= player->xvel;
-    boxRGBA(screen, player->xloc - (player->width)/2, player->yloc - (player->height)/2,
-                    player->xloc + (player->width)/2, player->yloc + (player->height)/2,
-                    0, 0, 255, 255);
+    SDL_Rect rect = {player->xloc-player->width/2-camera.xloc,player->yloc-(player->height)/2-camera.yloc,0,0};
+    SDL_BlitSurface(player_screen, NULL, screen, &rect);
+    /*boxRGBA(screen, player->xloc - (player->width)/2 - camera.xloc, player->yloc - (player->height)/2 - camera.yloc,
+                    player->xloc + (player->width)/2 - camera.xloc, player->yloc + (player->height)/2 - camera.yloc,
+                    0, 0, 255, 255);*/
 }
 
 int player_update(struct Player *player, SDL_Surface *screen)
@@ -236,15 +257,24 @@ int player_update(struct Player *player, SDL_Surface *screen)
             }
         }
     }
-    boxRGBA(screen, 0, 0, WIDTH, HEIGHT, 0, 0, 0, 255);
+    if (player->xloc < WIDTH/2) camera.xloc = 0;
+    else if (player->xloc > LEVEL_LENGTH - WIDTH/2) camera.xloc = LEVEL_LENGTH - WIDTH;
+    else camera.xloc = player->xloc - WIDTH/2;
+    boxRGBA(screen, 0, 0, WIDTH, HEIGHT, 0, 100, 100, 255);
 #if !defined(JUST_SHOW)
     struct FloorSection *F = floors;
+    SDL_Rect rect = {0, 0, 0, 0};
     for (i = 0; i < NUM_FLs; ++ i, ++ F)
     {
         if (F->view != 0xCAFEBAB2) break;
-        boxRGBA(screen, F->xloc, F->yloc, F->xloc + F->length, HEIGHT, 100, 0, 100, 255);
+        rect.x = F->xloc-camera.xloc; rect.y = F->yloc-camera.yloc;
+        SDL_BlitSurface(F->surf, NULL, screen, &rect);
+        //boxRGBA(screen, F->xloc, F->yloc, F->xloc + F->length, HEIGHT, 100, 0, 100, 255);
     }
 #endif
+    rect.x = EXIT_XLOC - camera.xloc - door_screen->w/2;
+    rect.y = EXIT_YLOC +1 - camera.yloc - door_screen->h;
+    SDL_BlitSurface(door_screen, NULL, screen, &rect);
     player_draw(player, screen);
 #if !defined(JUST_SHOW)
 //    assert(player_get_pos(player) == proper_get_pos(player, All_ticks));
@@ -254,7 +284,7 @@ int player_update(struct Player *player, SDL_Surface *screen)
 #else
                           proper_get_pos(player, All_ticks);
 #endif
-    if (player->xloc > WIDTH-30) return 2;
+    if (player->xloc > LEVEL_LENGTH-30) return 2;
     if (player->yloc - player->height/2 > HEIGHT) return 0;
 #if !defined(NO_FIREBALLS)
     for (i = 0; i < NUM_FBs; ++ i)
@@ -267,7 +297,7 @@ int player_update(struct Player *player, SDL_Surface *screen)
         Uint32 where = fireball_get_pos(&(fireballs[i]), All_ticks);
         Sint32 x = where>>16;
         Sint32 y = where&0xFFFF;
-        SDL_Rect rect = {x-5, y-5, 10, 10};
+        SDL_Rect rect = {x-5-camera.xloc, y-5-camera.yloc, 10, 10};
         SDL_BlitSurface(fb_screen, NULL, screen, &rect);
     }
 #endif
@@ -308,15 +338,22 @@ void lose_game(SDL_Surface *screen, SDL_Surface *GOS)
     SDL_Delay(1000);
 }
 
+void gen_rns(struct Player *player)
+{
+    srand(time(0));
+    player->rn1 = 50 + (rand()%50);
+    player->rn2 = 10 + (rand()%30);
+    player->rn3 = 50 + (rand()%100);
+    player->path_length = WIDTH+player->rn2;
+    player->width = player_screen->w;
+    player->height = player_screen->h;
+    EXIT_YLOC = 200 + player->height/2 - get_jump_height(10, player->rn2);
+}
+
 int main()
 {
     SDL_Surface *screen, *game_over_screen, *win_screen;
     struct Player player = {100, 200, 0, 0, 30, 25, 0, 0, 0, 0};
-    srand(time(0));
-    player.rn1 = 50 + (rand()%100);
-    player.rn2 = 50 + (rand()%100);
-    player.rn3 = 50 + (rand()%100);
-    player.path_length = WIDTH+player.rn2;
     int cont, i;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -335,6 +372,14 @@ int main()
     win_screen = SDL_LoadBMP("imgs/win.bmp");
     fb_screen = SDL_LoadBMP("imgs/fireball.bmp");
     SDL_SetColorKey(fb_screen, SDL_SRCCOLORKEY, SDL_MapRGB(fb_screen->format, 255, 0, 255));
+    floor_screen = SDL_LoadBMP("imgs/floor.bmp");
+    door_screen = SDL_LoadBMP("imgs/door.bmp");
+    SDL_SetColorKey(door_screen, SDL_SRCCOLORKEY, SDL_MapRGB(door_screen->format, 255, 0, 255));
+    player_screen = SDL_LoadBMP("imgs/player.bmp");
+    SDL_SetColorKey(player_screen, SDL_SRCCOLORKEY, SDL_MapRGB(player_screen->format, 255, 0, 255));
+
+    gen_rns(&player);
+
 #if !defined(JUST_SHOW)
     generate_floor(&player);
 
